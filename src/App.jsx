92a49d9769051ca056ea7c19e30cd8616a3c4b9a
saurav <sauravgpt123@gmail.com>
2023-11-27 11:34:38 +0530

    initial comit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Homepage from './screens/Homepage/Homepage'
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+
+
+  return (
+    <>
+      <Homepage/>
+    </>
+  )
+}
+
+export default App
