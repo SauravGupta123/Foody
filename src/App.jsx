a50b335af48a23d3acb8c760a3f4d1aa5b287c2f
saurav <sauravgpt123@gmail.com>
2023-12-19 18:43:21 +0530

    minor chnages

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import Homepage from './screens/Homepage/Homepage'
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import { CartProvider } from './components/contextReducer.jsx';
 
 function App() {
 
 
   return (
     <>
-      <Homepage/>
+      <CartProvider>
+        <Homepage />
+      </CartProvider>
+
     </>
   )
 }
