e2d8c4d94e1313217c32d735a5ab5ed3ed2c4e33
saurav <sauravgpt123@gmail.com>
2023-12-19 19:10:28 +0530

    .

--- src/screens/Homepage/Homepage.jsx
+++ src/screens/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../../components/Navbar';
-import Footer from '../../components/footer';
+import Footer from '../../components/Footer';
 import './Homepage.css';
 import Cards from '../../components/Cards';
 import Carausal from '../../components/Carausal';
