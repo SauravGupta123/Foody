879734a6ecc4b005a3a907c525900d188cbf7242
saurav <sauravgpt123@gmail.com>
2023-12-19 18:39:30 +0530

    both foodItems and foodCategory is fetched

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,27 @@
 const mongoose = require('mongoose');
 const fetchData = async () => {
     try {
-      const fetchedData = mongoose.connection.db.collection('foodData');
+      const foodItemsFetch =  mongoose.connection.db.collection('foodData');
+      const foodItemsData = await foodItemsFetch.find({}).toArray();
+
+      try{
+        const foodCatfetch =  mongoose.connection.db.collection('foodCategory');
+        const foodCatData = await foodCatfetch.find({}).toArray();
+        global.foodItems=foodItemsData;
+        global.foodCat=foodCatData;
+        
+
+      }
+      catch(err){
+          console.log("error fetching food category:", err);
+      }
+
   
-      // Using await to wait for the result of find().toArray()
-      const data = await fetchedData.find({}).toArray();
+
+      
+   
   
-      console.log('Data in the "foodData" collection:');
-      console.log(data);
-  
-      return data; // You can use this data as needed in your application
+
     } catch (err) {
       console.log('Error fetching data:', err);
       throw err; // Propagate the error if needed
@@ -21,7 +33,7 @@ const connectMongoose = async () => {
     const url = 'mongodb+srv://srv444:saurav123@gofooddb.ukrvosp.mongodb.net/goFoodDB?retryWrites=true&w=majority';
     await mongoose.connect(url);
     console.log('Connected to MongoDB');
-    // const foodData= await fetchData();
+    const foodData= await fetchData();
     // return foodData;
   
   } catch (err) {
